5f84332aae3474cf494adbae12da1e1be6ed6bd0
avinash1612 <56393465+avinash1612@users.noreply.github.com>
2020-10-04 01:48:08 +0530

    Update assi.py

--- assi.py
+++ assi.py
@@ -3,7 +3,9 @@ count = 0.0
 fh = open(fname)
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
-    a = line.find(" ")
+    a = line.find("a")
     b = float(line[a+2:])
-    count = count + b
+    count = count + b + c
 print("Done")
+d ="hello_world"
+#this is optimised code
